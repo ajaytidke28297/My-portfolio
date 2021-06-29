f10f829549dbd387228a23c013ad3662ba632446
ajaytidke28297 <ajaytidke28297@gmail.com>
2021-06-29 14:19:04 +0530

    About section added 😎😋

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,21 +2,18 @@ import React from "react";
 
 const About = ({ data }) => {
   if (data) {
-    var name = data.name;
     var profilepic = "images/" + data.image;
     var bio = data.bio;
-    var street = data.address.street;
-    var city = data.address.city;
-    var state = data.address.state;
-    var zip = data.address.zip;
-    var phone = data.phone;
-    var email = data.email;
     var resumeDownload = data.resumedownload;
   }
 
   return (
     <section id="about">
       <div className="row">
+        <div className="heading">
+          <h1>Nice to meet you</h1>
+          <h1>Nice to meet you</h1>
+        </div>
         <div className="three columns">
           <img
             className="profile-pic"
@@ -28,30 +25,18 @@ const About = ({ data }) => {
           <h2>About Me</h2>
 
           <p>{bio}</p>
-          <div className="row">
-            <div className="columns contact-details">
-              <h2>Contact Details</h2>
-              <p className="address">
-                <span>{name}</span>
-                <br />
-                <span>
-                  {street}
-                  <br />
-                  {city} {state}, {zip}
-                </span>
-                <br />
-                <span>{phone}</span>
-                <br />
-                <span>{email}</span>
-              </p>
-            </div>
-            <div className="columns download">
-              <p>
-                <a href={resumeDownload} className="button">
-                  <i className="fa fa-download"></i>Download Resume
-                </a>
-              </p>
-            </div>
+          <div className="columns download">
+            <p>
+              <a
+                href={resumeDownload}
+                className="button"
+                target="_blank"
+                rel="noopener noreferrer"
+                download
+              >
+                <i className="fa fa-download"></i>Download Resume
+              </a>
+            </p>
           </div>
         </div>
       </div>
